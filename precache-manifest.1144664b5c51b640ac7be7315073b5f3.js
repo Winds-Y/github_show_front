6fe5b59324806729be5c79a09d5a30628480e55c
yangtse <2155513297@qq.com>
2019-05-16 16:46:23 +0800

    Updates

---
+++ precache-manifest.1144664b5c51b640ac7be7315073b5f3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3bf80cec071476fddf25d173793c508",
+    "url": "/github_show_front/index.html"
+  },
+  {
+    "revision": "3ba01efa565e836016ba",
+    "url": "/github_show_front/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "4c5dff928ed47195f45c",
+    "url": "/github_show_front/static/css/main.04c3c42e.chunk.css"
+  },
+  {
+    "revision": "3ba01efa565e836016ba",
+    "url": "/github_show_front/static/js/2.debdbab5.chunk.js"
+  },
+  {
+    "revision": "4c5dff928ed47195f45c",
+    "url": "/github_show_front/static/js/main.8be23111.chunk.js"
+  },
+  {
+    "revision": "473a59614356caca80f3",
+    "url": "/github_show_front/static/js/runtime~main.21ec8f80.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github_show_front/static/media/logo.5d5d9eef.svg"
+  }
+]);
